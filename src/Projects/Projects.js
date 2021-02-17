a79c643668096d799fae3bffc833c7b8e03b61c2
Alan Turing <freemasl.sf@gmail.com>
2021-02-17 15:27:58 -0700

    Setup projects fn and add filler text

---
+++ src/Projects/Projects.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section className='projects'>
+      <h2>Projects</h2>
+      <article className='project'>
+        <h3>Project One</h3>
+        <p>It's rather a pity, in a way...now the universe is down to 699 wonders. Butterfingers. Now, don't antagonize her. I love a happy medium! Planets come and go. Stars perish. Matter disperses, coalesces, forms into other patterns, other worlds. Nothing can be eternal. Alright? Of course I'm alright, my child. You know, I am so constantly outwitting the opposition, I tend to forget the delights and satisfaction of the gentle art of fisticuffs. You realise one of us is lying about our basic programming. And I think we both know which one that is.</p>
+      </article>
+      <article className='project'>
+        <h3>Project Two</h3>
+        <p>Father Christmas. Santa Claus. Or, as I’ve always known him, Jeff. You may be a doctor. But I'm the Doctor. The definite article, you might say. I need...I need...I need... fish fingers and custard! I never know why. I only know who. Oh I'm so sorry, Jamie. Logic, my dear Zoe, merely enables one to be wrong with authority. Shut up! Just shut up, shut up, shut up, shuttity up up up! Planets come and go. Stars perish. Matter disperses, coalesces, forms into other patterns, other worlds. Nothing can be eternal.</p>
+      </article>
+      <article className='project'>
+        <h3>Project Three</h3>
+        <p>Your wish is my command. But be careful what you wish for. Oh my word! Please just... just see me. It’s a bit more exciting when you go the other way. Allons-y! This stuff, or whoever sent it, is cleverer than we are. Unfortunate, isn't it? People who talk about infallibility are usually on very shaky ground. Subject for catalogue: file under 'imagination, comma, lack of'. One day, just one day, maybe I'll meet somebody who gets the whole 'don't wander off' thing. Yeah? Well I'm the Lord of Time.</p>
+      </article>
+    </section>
+  )
+}
+
+export default Projects;
