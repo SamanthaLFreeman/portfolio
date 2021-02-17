53c661658e2aba61c0a488233189db190f6da7f2
Alan Turing <freemasl.sf@gmail.com>
2021-02-17 15:30:18 -0700

    Import About, Resume, Contact, and Projects modules in App

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import About from '../About/About';
+import Contact from '../Contact/Contact';
+import Projects from '../Projects/Projects';
+import Resume from '../Resume/Resume';
 
 class App extends Component {
   constructor() {
@@ -13,6 +16,9 @@ class App extends Component {
       <div className='App'>
         <h1 className='app-title'>Portfolio</h1>
         <About />
+        <Contact />
+        <Projects />
+        <Resume />
       </div>
     )
   }
