215d7e11fa091e8b7628d41169c5b4425201df0a
Alan Turing <freemasl.sf@gmail.com>
2021-02-22 16:01:10 -0700

    Add initial html structure for contact form

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,6 +4,21 @@ const Contact = () => {
   return (
     <section className='contact'>
       <h2>Contact me</h2>
+      <form className='contact-form'>
+        <input
+          placeholder='Name'
+          type='text'
+          name='name' />
+        <input
+          placeholder='Email'
+          type='text'
+          name='email' />
+        <input
+          placeholder='Tell me something...'
+          type='text'
+          name='content' />
+        <button className='contact-btn'>Submit</button>
+      </form>
     </section>
   )
 }
