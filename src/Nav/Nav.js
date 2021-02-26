c6153fd03ffa109124adcb579210d1ec8276c60e
Alan Turing <freemasl.sf@gmail.com>
2021-02-26 10:31:57 -0700

    Add buttons for navigation

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Nav = () => {
   return (
     <section className='nav'>
-      
+      <button className='nav-about'>About</button>
+      <button className='nav-projects'>Projects</button>
+      <button className='nav-resume'>Resume</button>
+      <button className='nav-contact'>Contact</button>
     </section>
   )
 }
